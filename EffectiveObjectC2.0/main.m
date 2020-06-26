3bb80bdc50d12fe85043e9f3193de3fa0539192b
jax <wzg_ios@163.com>
2020-06-26 22:49:20 +0800

    Initial Commit

---
+++ EffectiveObjectC2.0/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EffectiveObjectC2.0
+//
+//  Created by 伍兆贵 on 2020/6/26.
+//  Copyright © 2020 伍兆贵. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
